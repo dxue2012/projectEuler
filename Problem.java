151cf214adc35361a60010fef5a82d153f55b647
Dyland Xue <dylandxue2012@gmail.com>
2014-01-01 22:57:11 +0800

    added description in template file

--- Problem.java
+++ Problem.java
@@ -1,14 +1,16 @@
 /*
  * Problem X Solution
  *
+ * Solution template
+ *
  * @author Dyland Xue
  * @version 12/24/13
- *
  */
 
 public class ProblemX
 {
     public static void main(String[] args)
     {
+        System.out.println("This is problem No.X");
     }
 }
